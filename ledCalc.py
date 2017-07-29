0b0cf57edf79ac6fdf2f57dfcb99e828bb781fc2
H.G. Shoemaker <hgshoemaker@gmail.com>
2017-07-29 14:41:12 -0500

    Add files via upload
    Python 3 calculates current limiting resistor for LED

---
+++ ledCalc.py
@@ -0,0 +1,12 @@
+print("LED RESISTOR CALCULATOR")
+resistor = 0
+sourceV = int(input('Input supply voltage: '))
+fwdV = int(input('Input forward voltage eg 2: '))
+currentI = float(input('Input LED current in default [20mA] : ')or '20')
+
+currentI = currentI / 1000
+
+resistor = (sourceV - fwdV)/ currentI
+
+print()
+print('The resistor should be near {} ohms'.format(resistor))
